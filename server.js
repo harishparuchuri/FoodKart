fa211bc724e52906ad68614b47a94dcebbe10d13
harishparuchuri <harishparuchuri007@gmail.com>
2020-05-14 15:28:48 +0530

    completed

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory //
-app.use(express.static(__dirname + '/dist/FOODKART'));
+app.use(express.static(__dirname + '/dist/FoodKart'));
 
 // Start the app by listening on the default Heroku port //
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/FOODKART/index.html'));
+res.sendFile(path.join(__dirname+'/dist/FoodKart/index.html'));
 });
 
