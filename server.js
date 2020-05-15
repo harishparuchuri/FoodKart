fc3e2fcd3cafc217ddf610c37e676d2d7f7728e9
harishparuchuri <harishparuchuri007@gmail.com>
2020-05-15 14:05:12 +0530

    done

--- server.js
+++ server.js
@@ -16,14 +16,5 @@ res.sendFile(path.join(__dirname+'/dist/FoodKart/index.html'));
 });
 
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
